c0c56ddeb9b15b48e23cabc58401699dddad7b1b
Sam <sayhi@samvieten.com>
2015-12-07 08:01:47 +0100

    add futurity v 1.0
    - del old files
    - available options for detection
    - new code base, cleanup and prototype syntax

--- futuryfy.js
+++ futuryfy.js
@@ -1,101 +1,110 @@
-(function(){
-  var futuryfy = { 
-    init: function(){
-      //Options  
-      var futuryfymsg_headline = 'Outdated Browser';   
-      var futuryfymsg_content = 'Your Browser does not support modern web-standards. In order to display web content correctly and in fact of security concerns consider to update to a modern web-browser.';
-      //modal creation
-      if(this.webgl_detect() != true){
-        if(sessionStorage.getItem("futuryfyDisabled")){
-          return false;
-        }else if(document.cookie.indexOf("futuryfyDisabled")  >= 0){
-          return false;    
-        }else{
-          this.create( futuryfymsg_headline, futuryfymsg_content );
-        }
-      }  
-    },
-    create: function(headline, message){
-      var newEl = document.createDocumentFragment();
-      var modalCancel = document.createElement("a");
-          modalCancel.id = "futuryfy-cancel";
-          modalCancel.href = "#";
-          modalCancel.addEventListener('click',function(e){
-              futuryfy.cancel(e);
-          });
-          modalCancel.innerHTML = "Do not show again";
-      var modalCancelX = document.createElement("a");
-          modalCancelX.id = "futuryfy-cancel-x";
-          modalCancelX.href = "#";
-          modalCancelX.addEventListener('click',function(e){
-              futuryfy.cancelX(e);
-          });
-          modalCancelX.innerHTML = "&#10006;";
-      var modal = document.createElement("div");
-          modal.id = "futuryfy-modal";      
-          modal.innerHTML = '<div id="futuryfy-content">' + 
-                            '<strong>' + headline + '</strong>' + 
-                            '<p>' + message + '</p>' + 
-                            '<div id="futuryfy-action-bar">' +
-                            '<a href="" class="further-information">Further information</a>' +
-                            '</div></div>';  
-       newEl.appendChild(modal);
-       document.getElementsByTagName('body')[0].appendChild(modal);
-       document.getElementById('futuryfy-action-bar').appendChild(modalCancel);
-       var container = document.getElementById('futuryfy-content');
-       container.insertBefore(modalCancelX, container.childNodes[0]);
-       if(window.jQuery){
-         var jqEl = $("#futuryfy-modal");
-         jqEl.hide().fadeIn(500);
-       }
-     },
-     cancel: function(e){
-       e.stopPropagation();
-       e.preventDefault();
-       if(window.jQuery){$('#futuryfy-content').fadeOut(200,function(){$(this).add('#futuryfy-modal').remove();})}else{
-         var el = document.getElementById('futuryfy-modal');
-         el.parentNode.removeChild(el);
-       }
-       if(typeof window.sessionStorage != 'undefined'){//IF SESSIONSTORAGE IS AVAILABLE
-         sessionStorage.setItem('futuryfyDisabled', 'disabled');
-       }else{//ELSE COOKIE(FALLBACK)  
-         var now = new Date();
-         var time = now.getTime();
-         var expireTime = time + 1000*60;
-         now.setTime(expireTime);
-         document.cookie = 'cookie=futuryfyDisabled;expires='+now.toGMTString()+';path=/';
-       }
-      return;
-    },
-    cancelX: function(e){
-       e.stopPropagation();
-       e.preventDefault();
-       if(window.jQuery){$('#futuryfy-content').fadeOut(200,function(){$(this).add('#futuryfy-modal').remove();})}else{
-         var el = document.getElementById('futuryfy-modal');
-         el.parentNode.removeChild(el);
-       }
-       return;
-    },
-    webgl_detect: function(return_context){//Check if WebGL is available (modern Browser)
-      if(window.WebGLRenderingContext){
-        var canvas = document.createElement("canvas"),
-            names = ["webgl", "experimental-webgl", "moz-webgl", "webkit-3d"],
-            context = false;
-        for(var i=0;i<4;i++) {
-            try {
-                context = canvas.getContext(names[i]);
-                if (context && typeof context.getParameter == "function") {
-                    if (return_context) {
-                        return {name:names[i], gl:context};
+var fy = (futuryfy = {
+    app: 'futuryfy',
+    author: 'sam vieten',
+    detection : {
+        soft: function(modal) {
+            var result =     this.support.webgl();
+            !result && modal ? this.modal() : false;
+            return result;
+        },
+        modern: function(modal) {
+            var result =    this.support.webgl() &&
+                            this.support.htmlTemplate();
+            !result && modal ? this.modal() : false;
+            return result;
+        },
+        advanced: function(modal) {
+            var result =    this.support.webgl() &&
+                            this.support.htmlTemplate() &&
+                            this.support.customElements();
+            !result && modal ? this.modal() : false;
+            return result;
+        },
+        extreme: function(modal) {
+            var result =    this.support.webgl() &&
+                            this.support.htmlTemplate() &&
+                            this.support.customElements() &&
+                            this.support.animationFrame() &&
+                            this.support.htmlImport();
+            !result && modal ? this.modal() : false;
+            return result;
+        },
+        modal: function(message, styles) {
+            message ? false : message = 'Your browser seems to be outdated. Please upgrade to a more modern browser ~ futuryfy.js'
+            var modal = document.createElement('div'),
+                modalClose = document.createElement('button');
+            modal.style.display = 'block';
+            modal.style.position = 'absolute';
+            modal.style.top = '0';
+            modal.style.width = '100%';
+            modal.style.boxSizing = 'border-box';
+            modal.style.height = 'auto';
+            modal.style.padding = '10px';
+            modal.style.backgroundColor = '#c00';
+            modal.style.color = '#fff';
+            modal.style.fontFamily = '"Helvetica", sans-serif';
+            modal.className = 'fy-modal';
+            modalClose.style.float = 'right';
+            modalClose.innerHTML = 'x';
+            modal.innerHTML = message;
+            modal.appendChild(modalClose);
+            modalClose.addEventListener('click', function(e){
+                modal.remove();
+            });
+            document.body.appendChild(modal);
+        },
+        //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+        support: {
+            webgl: function(return_context) {
+                if (!!window.WebGLRenderingContext) {
+                    var canvas = document.createElement("canvas"),
+                         names = ["webgl", "experimental-webgl","moz-webgl", "webkit-3d"],
+                         context = false;
+                    for(var i=0;i<4;i++) {
+                        try {
+                            context = canvas.getContext(names[i]);
+                            if (context && typeof context.getParameter == "function") {
+                                if (return_context) {
+                                    return {name:names[i], gl:context};
+                                }
+                                return true;
+                            }
+                        } catch(e) {}
                     }
-                    return true;
+                    return false;
                 }
-            } catch(e) {}
+                return false;
+            },
+            htmlTemplate: function() {
+                return 'content' in document.createElement('template');
+            },
+            customElements: function() {
+                return 'registerElement' in document;
+            },
+            shadowDOM: function() {
+                return document.head.createShadowRoot;
+            },
+            htmlImport: function() {
+                return 'import' in document.createElement('link');
+            },
+            animationFrame: function() {
+                return window.requestAnimationFrame;
+            }
         }
-        return "disabled";
-      }
-      return false;
     }
-  };
-  futuryfy.init();
-})();
+});
+fy.detection.advanced('true')
+/*
+fy.detection.soft()
+fy.detection.modern()
+fy.detection.advanced()
+fy.detection.extreme()
+fy.detection.support.webgl()
+fy.detection.support.htmlTemplate()
+fy.detection.support.customElements()
+fy.detection.support.shadowDOM()
+fy.detection.support.htmlImport()
+fy.detection.support.animationFrame()
+
+options: true / false @modal
+*/
